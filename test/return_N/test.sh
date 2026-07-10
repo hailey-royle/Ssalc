31bfd9a41a6a89c708422bdb20d5e31f205dd4f5
Hailey Royle <hailey.royle@proton.me>
2026-07-10 11:05:14 -0600

    test template and cleaning test data

--- test/return_N/test.sh
+++ test/return_N/test.sh
@@ -1,9 +1,19 @@
+# compile the test program to test.sl, redirect any errors or warnings to out.txt and stop if error.
 ../../ssalc test.sl >out.txt &&
-clang out.ll ../_start.o -nostdlib -static -o test &&
-rm out.ll &&
-./test;
+
+# compile out.ll to test, normal flags, stop if error
+clang out.ll ../_start.o -nostdlib -static -o test >>out.txt &&
+
+# run the test
+./test
+
+# evaulate if the test passed and print the result
 if [ $? -eq 173 ]; then
 	echo -e "${PWD##*/} [ \e[32mpassed\e[0m ]"
 else
 	echo -e "${PWD##*/} [ \e[31mfailed\e[0m ]"
 fi
+
+# clean directory
+rm out.txt out.ll test >/dev/null 2>/dev/null
+
--- test/return_argument.count/test.sh
+++ test/return_argument.count/test.sh
@@ -1,10 +1,19 @@
+# compile the test program to test.sl, redirect any errors or warnings to out.txt and stop if error.
 ../../ssalc test.sl >out.txt &&
-clang out.ll ../_start.o -nostdlib -static -o test &&
-rm out.ll &&
-./test two three four
+
+# compile out.ll to test, normal flags, stop if error
+clang out.ll ../_start.o -nostdlib -static -o test >>out.txt &&
+
+# run the test
+./test two 3 foiuas0d8h0a9
+
+# evaulate if the test passed and print the result
 if [ $? -eq 4 ]; then
 	echo -e "${PWD##*/} [ \e[32mpassed\e[0m ]"
 else
 	echo -e "${PWD##*/} [ \e[31mfailed\e[0m ]"
 fi
+
+# clean directory
+rm out.txt out.ll test >/dev/null 2>/dev/null
 
