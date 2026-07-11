0eaf079e048bf089ebe3d4dd158b9c26fd13f27a
Hailey Royle <hailey.royle@proton.me>
2026-07-10 19:37:24 -0600

    improve test error messages

--- test/return_N/test.sh
+++ test/return_N/test.sh
@@ -1,19 +1,24 @@
-# compile the test program to test.sl, redirect any errors or warnings to out.txt and stop if error.
-../../ssalc test.sl >out.txt &&
+# compile the test program to test.sl, redirect any errors to out.txt and report any error.
+../../ssalc test.sl >out.txt
+if [ $? != 0 ]; then
+	echo -e "${PWD##*/} [ \e[31mssalc failed\e[0m ]"
+	exit
+fi
 
-# compile out.ll to test, normal flags, stop if error
-clang out.ll ../_start.o -nostdlib -static -o test >>out.txt &&
+# compile out.ll to test with normal flags, redirect any errors to out.txt and report any error
+clang out.ll ../_start.o -nostdlib -static -o test >>out.txt
+if [ $? !=  0 ]; then
+	echo -e "${PWD##*/} [ \e[31mclang failed\e[0m ]"
+	exit
+fi
 
 # run the test
 ./test
-
-# evaulate if the test passed and print the result
-if [ $? -eq 173 ]; then
+if [ $? == 173 ]; then
 	echo -e "${PWD##*/} [ \e[32mpassed\e[0m ]"
 else
-	echo -e "${PWD##*/} [ \e[31mfailed\e[0m ]"
+	echo -e "${PWD##*/} [ \e[31mprogram failed\e[0m ]"
 fi
 
 # clean directory
 rm out.txt out.ll test >/dev/null 2>/dev/null
-
--- test/return_argument.count/test.sh
+++ test/return_argument.count/test.sh
@@ -1,19 +1,24 @@
-# compile the test program to test.sl, redirect any errors or warnings to out.txt and stop if error.
-../../ssalc test.sl >out.txt &&
+# compile the test program to test.sl, redirect any errors to out.txt and report any error.
+../../ssalc test.sl >out.txt
+if [ $? != 0 ]; then
+	echo -e "${PWD##*/} [ \e[31mssalc failed\e[0m ]"
+	exit
+fi
 
-# compile out.ll to test, normal flags, stop if error
-clang out.ll ../_start.o -nostdlib -static -o test >>out.txt &&
+# compile out.ll to test with normal flags, redirect any errors to out.txt and report any error
+clang out.ll ../_start.o -nostdlib -static -o test >>out.txt
+if [ $? !=  0 ]; then
+	echo -e "${PWD##*/} [ \e[31mclang failed\e[0m ]"
+	exit
+fi
 
 # run the test
-./test two 3 foiuas0d8h0a9
-
-# evaulate if the test passed and print the result
-if [ $? -eq 4 ]; then
+./test two 3 fourlkasjdfohaspdufaspidfuaipsdugf8as7dgf9adfp
+if [ $? == 4 ]; then
 	echo -e "${PWD##*/} [ \e[32mpassed\e[0m ]"
 else
-	echo -e "${PWD##*/} [ \e[31mfailed\e[0m ]"
+	echo -e "${PWD##*/} [ \e[31mprogram failed\e[0m ]"
 fi
 
 # clean directory
 rm out.txt out.ll test >/dev/null 2>/dev/null
-
