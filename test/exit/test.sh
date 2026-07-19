b2dd3d484e82f6b0565dced358d70b0ca85d8ef6
Hailey Royle <hailey.royle@proton.me>
2026-07-19 11:20:57 -0600

    fixed .gitignore not finding new test and add exit call test

---
+++ test/exit/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# clean directory
+rm out.txt test.ll test &>/dev/null
+
+# compile test.sl to test.ll, redirect any errors to out.txt.
+../../ssalc test.sl &>out.txt
+if [ $? != 0 ]; then
+	echo -e "${PWD##*/} \t[\e[31m ssalc failed \e[0m]"
+	exit
+fi
+
+# compile test.ll to test with normal flags, redirect any errors to out.txt.
+clang test.ll ../../_start.o -nostdlib -static -o test &>out.txt
+if [ $? !=  0 ]; then
+	echo -e "${PWD##*/} \t[\e[31m clang failed \e[0m]"
+	exit
+fi
+
+# run the test
+./test
+if [[ $? == 30 ]]; then
+	echo -e "${PWD##*/} \t[\e[32m passed \e[0m]"
+else
+	echo -e "${PWD##*/} \t[\e[31m executable failed \e[0m]"
+fi
+
+# clean directory
+rm out.txt test.ll test &>/dev/null
---
+++ test/exit/test.sl
@@ -0,0 +1,5 @@
+start procedure[ argument @@i8 : i64 ]
+{
+	exit[ 30 ];
+	!return[ 31 ];
+}
