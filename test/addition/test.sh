d6b3fcb3888c66abdaf271e6977db9e71190a2ef
Hailey Royle <hailey.royle@proton.me>
2026-07-20 10:13:50 -0600

    added addition test

---
+++ test/addition/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+output () {
+	if [ "$1" == "passed" ]; then
+		printf "%-*s [\e[32m %s \e[0m]\n" 24 "${PWD##*/}" "$1"
+		rm out.txt test.ll test &>/dev/null
+	else
+		printf "%-*s [\e[31m %s \e[0m]\n" 24 "${PWD##*/}" "$1"
+	fi
+	exit
+}
+
+# clean directory
+rm out.txt test.ll test &>/dev/null
+
+# compile test.sl to test.ll, redirect any errors to out.txt.
+../../ssalc test.sl &>out.txt
+if [ $? != 0 ]; then
+	output "ssalc failed"
+fi
+
+# compile test.ll to test with normal flags, redirect any errors to out.txt.
+clang test.ll ../../_start.o -nostdlib -static -o test &>out.txt
+if [ $? != 0 ]; then
+	output "clang failed"
+fi
+
+# run the test
+./test
+if [ $? == 19 ]; then
+	output "passed"
+else
+	output "executable failed"
+fi
+
---
+++ test/addition/test.sl
@@ -0,0 +1,4 @@
+start procedure[ argument @@i8 : i64 ]
+{
+	!return[ 10 + 9 ];
+}
