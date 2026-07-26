23158a54349fb221405fd3af133ad5e4eb02cddf
Hailey Royle <hailey.royle@proton.me>
2026-07-26 14:50:06 -0600

    assert.h include unrechable

--- source/assert.h
+++ source/assert.h
@@ -7,8 +7,13 @@
 		fprintf( stderr, __VA_ARGS__ ); \
 		fprintf( stderr, "\r\n" ); \
 		fflush( stderr ); \
-		exit( 1 );\
+		exit( 1 ); \
 	} \
+}
+
+#define unreachable { \
+	fprintf( stderr, "%s:%d: %s: Unrechable reached.\r\n", __FILE__, __LINE__, __func__ ); \
+	exit( 1 ); \
 }
 
 #endif
